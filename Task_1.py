35b42a4633be685842daa45184eef5f395c2472d
Ninikopaliani <kopalianini@gmail.com>
2024-02-25 11:09:36 -0500

    first commit

---
+++ Task_1.py
@@ -0,0 +1,5 @@
+user_name= input("enter your name")
+User_lastname=input("enter your last name")
+user_age=input("enter how old are you")
+print("გამარჯობა",user_name,User_lastname,"თქვენ ხართ",user_age,"წლის")
+
