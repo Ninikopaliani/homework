d4b51895ef6e9be81eb9f2561d30e0650a134d6e
Ninikopaliani <kopalianini@gmail.com>
2024-02-25 11:30:21 -0500

    second task

---
+++ Task_2.py
@@ -0,0 +1,6 @@
+user_name=input("შეიყვანე სახელი")
+user_birth_year=input("შეიყვანე დაბადების წელი")
+user_birth_year1=int(user_birth_year)
+today_year=2024
+user_age=today_year-user_birth_year1
+print("გამარჯობა,",user_name,"თქვენ ხართ",user_age,"წლის")
